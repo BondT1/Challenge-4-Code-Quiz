b86f0598b4478498e9328f8d379801f75fd281cc
Tom Bond <tombond248@gmail.com>
2022-10-22 18:13:58 +0100

    further JS chnages to start quiz button

--- script.js
+++ script.js
@@ -1,10 +1,74 @@
 var homepageEl = document.getElementById("homepage");
 var questionViewEl = document.getElementById("question-view");
-var questionEl = document.getElementById("question")
-var answerEl = document.getElementById("answers")
-var ViewHighScoreEl = document.getElementById("view-high-scores");
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answers");
+var viewHighScoreEl = document.getElementById("view-high-scores");
 
+var startQuizBtn = document.querySelector("#start-quiz");
 var timerEl = document.querySelector("#timer");
 
+const questionEl = [
+  { 
+    question: 'question here',
+    answers: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+    ],
+    correct: 1,  
+  },
 
+  {
+    question: 'question here',
+    answers: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+    ],
+    correct: 1,  
+  },
+
+  { 
+    question: 'question here',
+    answers: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+    ],
+    correct: 1,  
+  },
+    
+  { 
+    question: 'question here',
+    answers: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+    ],
+    correct: 1,  
+  },
+        
+  { 
+    question: 'question here',
+    answers: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+    ],
+    correct: 1,  
+  },        
+];
+
+var startQuizBtn = function() {
+  homepageEl.classList.add("remove");
+  homepageEl.classList.remove("add");
+  questionViewEl.classList.add("add");
+  questionViewEl.classList.remove("remove");
+
+}
 
