4cc698242ca34b29c6e59aae5c8cbdccb9c53542
Tom Bond <tombond248@gmail.com>
2022-10-22 16:53:23 +0100

    questions CSS and JS variables

---
+++ script.js
@@ -0,0 +1,10 @@
+var homepageEl = document.getElementById("homepage");
+var questionViewEl = document.getElementById("question-view");
+var questionEl = document.getElementById("question")
+var answerEl = document.getElementById("answers")
+var ViewHighScoreEl = document.getElementById("view-high-scores");
+
+var timerEl = document.querySelector("#timer");
+
+
+
